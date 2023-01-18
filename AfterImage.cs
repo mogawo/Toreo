68867e551e1c0d0950464cf05f81f29fe61adf1e
Phuoc Le <54290976+Megagotnothing@users.noreply.github.com>
2023-01-18 11:43:24 -0800

    First Commit

---
+++ AfterImage.cs
@@ -0,0 +1,68 @@
+using Godot;
+using System;
+
+public class AfterImage : Node2D
+{
+    static public int colorIndex = 0;
+
+    [Export]
+    Color[] pallete = new Color[]{
+                                    Color.ColorN("red"),
+                                    Color.ColorN("orange"),
+                                    Color.ColorN("yellow"),
+                                    Color.ColorN("green"),
+                                    Color.ColorN("blue"),
+                                    Color.ColorN("indigo"),
+                                    Color.ColorN("violet")
+                                };
+
+    [Export]
+    Texture texture;
+
+    [Export]
+    float lifetime, opacity;
+
+    static public int allowedExisting = 7;
+
+    static public int currExisting = 0;
+
+    Sprite sprite;
+    Timer timer;
+
+    public override void _Ready()
+    {
+        currExisting++;
+        if (colorIndex >= pallete.Length)
+        {
+            colorIndex = 0;
+        }
+
+        sprite = GetNode<Sprite>("Sprite");
+        sprite.Texture = texture;
+        timer = GetNode<Timer>("Timer");
+
+        ShaderMaterial spriteMat = (sprite.Material as ShaderMaterial);
+
+        Color overlay = pallete[colorIndex];
+
+        overlay.a = 0;
+        spriteMat.SetShaderParam("color_overlay", overlay);
+        ++colorIndex;
+
+        sprite.Material = spriteMat;
+    }
+
+    public override void _Process(float delta)
+    {
+        
+        if(timer.IsStopped())
+        {
+            QueueFree();
+        }
+    }
+
+    public static bool isMaxedExisitng()
+    {
+        return currExisting == allowedExisting;
+    }
+}
